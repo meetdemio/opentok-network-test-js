8f0d854ebd59c61dfed2d1f2312a5f18fed33010
miKsansegundo <miguelsansegundo@gmail.com>
2019-05-16 14:23:41 +0700

    Allow to set audio/video device id that is used for testing opentok/opentok-network-test-js#96

--- src/NetworkTest/index.ts
+++ src/NetworkTest/index.ts
@@ -31,6 +31,8 @@ import OTKAnalytics = require('opentok-solutions-logging');
 export interface NetworkTestOptions {
   audioOnly?: boolean;
   timeout?: number;
+  audioSource?: string;
+  videoSource?: string;
 }
 
 export default class NetworkTest {
@@ -106,7 +108,7 @@ export default class NetworkTest {
         throw new InvalidOnUpdateCallback();
       }
     }
-
+    console.log('AQUI options', this.options)
     return testQuality(
       this.OT, this.credentials, this.otLogging, this.options, updateCallback);
   }
